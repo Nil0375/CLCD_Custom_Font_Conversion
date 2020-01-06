b7d06928368cae36bca89d3edd482bd460f42906
Nil0375 <khc0013579@gmail.com>
2020-01-07 00:38:32 +0800

    ADD Project File

---
+++ Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("微軟正黑體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button1.Location = New System.Drawing.Point(265, 222)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(90, 37)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "清除"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("微軟正黑體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button2.Location = New System.Drawing.Point(265, 265)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(90, 37)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "確認"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("微軟正黑體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button3.Location = New System.Drawing.Point(265, 308)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(90, 37)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "儲存"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(5, 353)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(350, 170)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(359, 526)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "自定義字型產生器"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,147 @@
+﻿Public Class Form1
+    Dim butt(7, 4) As Button
+    Dim butthex(7, 4) As Byte, line As Byte = 0
+    Dim linestr(7) As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        linestr(0) = "" : linestr(1) = "" : linestr(2) = "" : linestr(3) = "" : linestr(4) = "" : linestr(5) = "" : linestr(6) = ""
+        For j As Byte = 0 To 7
+            For i As Integer = 0 To 4
+                butt(j, i) = New Button
+                With butt(j, i)
+                    .Cursor = Cursors.Cross
+                    .BackColor = Color.White
+                    .FlatStyle = FlatStyle.Flat
+                    .ForeColor = Color.DarkGray
+                    .Left = 75 + (35 * i)
+                    .Top = 25 + (40 * j)
+                    .Width = 35
+                    .Height = 40
+                End With
+                Me.Controls.Add(butt(j, i))
+                AddHandler butt(j, i).Click, AddressOf A_ClickHandler
+
+                butt(j, i).Show()
+            Next
+        Next
+        linestr(7) = ";"
+    End Sub
+
+    Private Sub A_ClickHandler(sender As Object, e As EventArgs) Handles Me.Click
+        'Throw New NotImplementedException()
+        Try
+            If CType(sender, Button).BackColor = Color.Red Then
+                CType(sender, Button).BackColor = Color.White
+            Else
+                CType(sender, Button).BackColor = Color.Red
+            End If
+            For j As Byte = 0 To 7
+                For i As Byte = 0 To 4
+                    If butt(j, i).BackColor = Color.Red Then
+                        butthex(j, i) = 1
+                    Else
+                        butthex(j, i) = 0
+                    End If
+                Next
+            Next
+            Dim hexbuffer(7) As String
+            For i As Byte = 0 To 7
+                hexbuffer(i) = Convert.ToString((butthex(i, 0) * 16) + (butthex(i, 1) * 8) + (butthex(i, 2) * 4) + (butthex(i, 3) * 2) + (butthex(i, 4)), 16)
+            Next
+            Select Case line
+                Case 0
+                    linestr(0) = "{" & "0x" & hexbuffer(0) & "," & "0x" & hexbuffer(1) & "," & "0x" & hexbuffer(2) & "," & "0x" & hexbuffer(3) & "," & "0x" & hexbuffer(4) & "," & "0x" & hexbuffer(5) & "," & "0x" & hexbuffer(6) & "," & "0x" & hexbuffer(7) & "}"
+                Case 1 To 6
+                    linestr(line) = "," & vbCrLf & "{" & "0x" & hexbuffer(0) & "," & "0x" & hexbuffer(1) & "," & "0x" & hexbuffer(2) & "," & "0x" & hexbuffer(3) & "," & "0x" & hexbuffer(4) & "," & "0x" & hexbuffer(5) & "," & "0x" & hexbuffer(6) & "," & "0x" & hexbuffer(7) & "}"
+                Case 7
+                    linestr(7) = "," & vbCrLf & "{" & "0x" & hexbuffer(0) & "," & "0x" & hexbuffer(1) & "," & "0x" & hexbuffer(2) & "," & "0x" & hexbuffer(3) & "," & "0x" & hexbuffer(4) & "," & "0x" & hexbuffer(5) & "," & "0x" & hexbuffer(6) & "," & "0x" & hexbuffer(7) & "}" & linestr(7)
+            End Select
+            TextBox1.Text = linestr(0) & linestr(1) & linestr(2) & linestr(3) & linestr(4) & linestr(5) & linestr(6) & linestr(7)
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        For j As Byte = 0 To 7
+            For i As Byte = 0 To 4
+                butt(j, i).BackColor = Color.White
+            Next
+        Next
+        If line = 0 Then
+            linestr(0) = "{" & linestr(0)
+            linestr(7) = "}" & linestr(7)
+        End If
+        line = line + 1
+        Dim c As Boolean
+        If line > 7 Then
+            c = MsgBox("可字定義字型已達上限" & vbCrLf & "請先儲存檔案", 0 + 48, "Error")
+            line = 0
+        End If
+        If c = True Then
+            Dim FileNum As UInt16
+            Dim fileroad As String
+            Dim saveFileDialog1 As New SaveFileDialog()
+            saveFileDialog1.Filter = "Text|*.txt"
+            saveFileDialog1.Title = "Save an txt"
+            saveFileDialog1.ShowDialog()
+            If saveFileDialog1.FileName <> "" Then
+                fileroad = saveFileDialog1.FileName
+
+                FileNum = FreeFile()
+                FileOpen(FileNum, fileroad, OpenMode.Output)
+                PrintLine(FileNum, linestr(0) & linestr(1) & linestr(2) & linestr(3) & linestr(4) & linestr(5) & linestr(6) & linestr(7))
+                FileClose(FileNum)
+            End If
+            For j As Byte = 0 To 7
+                For i As Byte = 0 To 4
+                    butt(j, i).BackColor = Color.White
+                    linestr(j) = ""
+                Next
+            Next
+            TextBox1.Text = ""
+            line = 0
+        End If
+        TextBox1.Text = ""
+        TextBox1.Text = linestr(0) & linestr(1) & linestr(2) & linestr(3) & linestr(4) & linestr(5) & linestr(6) & linestr(7)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim FileNum As UInt16
+        Dim fileroad As String
+        Dim saveFileDialog1 As New SaveFileDialog()
+        saveFileDialog1.Filter = "Text|*.txt"
+        saveFileDialog1.Title = "Save an txt"
+        saveFileDialog1.ShowDialog()
+        If saveFileDialog1.FileName <> "" Then
+            fileroad = saveFileDialog1.FileName
+
+            FileNum = FreeFile()
+            FileOpen(FileNum, fileroad, OpenMode.Output)
+            PrintLine(FileNum, linestr(0) & linestr(1) & linestr(2) & linestr(3) & linestr(4) & linestr(5) & linestr(6) & linestr(7))
+            FileClose(FileNum)
+        End If
+        For j As Byte = 0 To 7
+            For i As Byte = 0 To 4
+                butt(j, i).BackColor = Color.White
+                linestr(j) = ""
+            Next
+        Next
+        TextBox1.Text = ""
+        line = 0
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For j As Byte = 0 To 7
+            For i As Byte = 0 To 4
+                butt(j, i).BackColor = Color.White
+            Next
+        Next
+        TextBox1.Text = ""
+        line = 0
+        For i As Integer = 0 To 7
+            linestr(i) = ""
+        Next
+        linestr(7) = ";"
+    End Sub
+End Class
